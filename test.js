269cadfbdeb511d66b1279ae9a0c9c5525145c57
bert.degeyter <bert.degeyter@showpad.com>
2024-05-24 09:10:27 +0200

    Include suggested jest polyfills

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 const { setupServer } = require("msw/node");
 const { http, HttpResponse } = require("msw");
-const { fetch } = require("undici");
+const { fetch } = require("undici"); // uncomment this line to make the test pass
 
 const server = setupServer();
 
@@ -19,9 +19,7 @@ afterAll(() => server.close());
 describe("Test", () => {
   it("should pass", async () => {
     server.use(
-      http.get("https://example.com", () => {
-        return HttpResponse.text("Hello World");
-      })
+      http.get("https://example.com", () => HttpResponse.text("Hello World"))
     );
     const result = await fetch("https://example.com").then((r) => r.text());
     expect(result).toBe("Hello World");
